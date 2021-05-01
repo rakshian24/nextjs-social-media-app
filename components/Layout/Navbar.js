d961ea4c9b18122adea7be4cded34362be25541b
rakshsindhe <35263835+rakshsindhe@users.noreply.github.com>
2021-05-01 17:36:23 +0530

    Added Navbar with routes

--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React from 'react';
+import { Menu, Container, Icon } from 'semantic-ui-react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 function Navbar() {
-  return <div></div>;
+  const router = useRouter();
+
+  const isActive = (route) => {
+    return router.pathname === route;
+  };
+
+  return (
+    <Menu fluid borderless>
+      <Container text>
+        <Link href="/login">
+          <Menu.Item header active={isActive('/login')}>
+            <Icon size="large" name="sign in" />
+            Login
+          </Menu.Item>
+        </Link>
+        <Link href="/signup">
+          <Menu.Item header active={isActive('/signup')}>
+            <Icon size="large" name="signup" />
+            Sign up
+          </Menu.Item>
+        </Link>
+      </Container>
+    </Menu>
+  );
 }
 
 export default Navbar;
