6f9d50d36442d14c6d721c38fe92430c605d1683
rakshsindhe <35263835+rakshsindhe@users.noreply.github.com>
2021-05-02 04:08:52 +0530

    Added getInitialProps

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
 
-const Index = () => {
-    return (
-        <div>
-            HomePage
-        </div>
-    )
-}
+const Index = ({ posts }) => {
+  return (
+    <>
+      <h1>HomePage</h1>
+      <div>
+        {posts &&
+          posts.length > 0 &&
+          posts.map((post) => <p key={post._id}>{post.title}</p>)}
+      </div>
+    </>
+  );
+};
 
-export default Index
+/* getInitialProps are used to get any data before the page loads. 
+   Here we can see that the posts data is fetched before the Index page loads and 
+   then the Index page can avail the post data*/
+Index.getInitialProps = async (ctx) => {
+  try {
+    const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    return { posts: res.data };
+  } catch (error) {
+    return { errorLoading: true };
+  }
+};
+
+export default Index;
